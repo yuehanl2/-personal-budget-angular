13a1f7b6737b7f797de137c5aaf557b5968297c5
ylan2 <ylan2@uncc.edu>
2020-10-03 23:11:58 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -2,33 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use('/', express.static('public'));
 
-const budget = {
-    
-    
-    myBudget: [
-    {
-        title:'Eat out',
-        budget:25
-    },
-    {
-        title:'Rent',
-        budget:375
-    },
-    {
-        title:'Groceries',
-        budget:110
-    },
-]
-};
+//fetch("./budget.json")
+
+app.use('/', express.static('public'));
 
 app.get('/hello', (req, res) =>  {
     res.send('Hello World!');
 });
 
 app.get('/budget',(req, res)=>{
-    res.json(budget);
+    res.sendFile(__dirname+'/budget.json');
 });
 
 app.listen(port, () => {
