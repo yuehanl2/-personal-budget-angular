088b373bf852d0405ec806941812f9bbf0da54bc
ylan2 <ylan2@uncc.edu>
2020-10-03 20:05:08 -0400

    Added server, Axios, and ChartJS

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use('/', express.static('public'));
+
+const budget = {
+    
+    
+    myBudget: [
+    {
+        title:'Eat out',
+        budget:25
+    },
+    {
+        title:'Rent',
+        budget:375
+    },
+    {
+        title:'Groceries',
+        budget:110
+    },
+]
+};
+
+app.get('/hello', (req, res) =>  {
+    res.send('Hello World!');
+});
+
+app.get('/budget',(req, res)=>{
+    res.json(budget);
+});
+
+app.listen(port, () => {
+    console.log(`Serving at http://localhost:${port}`)
+  });
